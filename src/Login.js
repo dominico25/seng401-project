2192ff129d9a92a706c781f1ed31f36b0da0a046
Shahdad20 <kaaj1350@gmail.com>
2024-02-09 20:37:37 -0700

    Shahdad - Added Login portion

--- src/Login.js
+++ src/Login.js
@@ -1,13 +1,29 @@
-import { useState } from "react";
-import React from "react";
-import Navigation from './Navigation';
+import React from 'react';
+import { GoogleLogin } from 'react-google-login';
 
-function Login() {
-    return (
-        <header>
-            <Navigation/>
-        </header>
-    );
-}
+const clientId = '89532782253-gsmotckp4f5jcjv3mu1c1k1sk6od2pqa.apps.googleusercontent.com'; 
+
+const Login = () => {
+  const onSuccess = (response) => {
+    console.log('Login Success:', response.profileObj);
+  };
+
+  const onFailure = (error) => {
+    console.error('Login Failure:', error);
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <GoogleLogin
+        clientId={clientId}
+        buttonText="Login with Google"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={'single_host_origin'}
+      />
+    </div>
+  );
+};
 
 export default Login;
