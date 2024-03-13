ea8d5c8ddf5e766486730e0ec1d7c89e6b77aa79
Raina <raina.jugdev@ucalgary.ca>
2024-03-13 10:53:50 -0600

    Fixing login

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { useNavigate } from "react-router-dom"; // Import useNavigate
 import './home.css';
 import Header from "./Header";
+import { googleLogout } from '@react-oauth/google'; // Import googleLogout
 
 function Home(){
+    const navigate = useNavigate(); // Get the navigate function
+
+    // Define a logout function
+    const logout = () => {
+        googleLogout(); // Logout using googleLogout
+        navigate('/'); // Navigate back to the '/Main' page
+    };
+
     return (
         <div>
             <Header />
             <h1>Home</h1>
+            <button onClick={logout}>Logout</button> {/* Logout button */}
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <GoogleOAuthProvider clientId="52196466868-3n2nemknh184a6nsp6vp900g77jqd0ak.apps.googleusercontent.com">
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </GoogleOAuthProvider>
+  </>
 );
 
 // If you want to start measuring performance in your app, pass a function
